43ae22f24c614b7d77e74fd860dcefb6549c58d7
nur <77068320+mirror-voly@users.noreply.github.com>
2024-07-19 19:02:27 +0400

    OnboardPage Fully functional

--- Sport Manager/OnboardViewPresenter.swift
+++ Sport Manager/OnboardViewPresenter.swift
@@ -13,36 +13,66 @@ struct OnboardViewPresenter: View {
     @State var viewText = ""
     @State var currentView = 0
     
+    let onboarsViews: [OnboardView] = []
     
-    let onboarsViews: [OnboardView] = [OnboardView(viewTitle: "App for the journalists", viewText:                                      "Add and manage your arcticles", viewImageName:
-                                        "OnboardImageOne"),
-                                       OnboardView(viewTitle: "Manage your budget", viewText: "Add your statistics and earn more", viewImageName: "OnboardImageTwo"),
-                                        OnboardView(viewTitle: "Smart notes", viewText: "Integrated notes to help you in the events", viewImageName: "OnboardImageThree")]
-    
+    init() {
+        UIPageControl.appearance().currentPageIndicatorTintColor  = UIColor(Color(.onboardColorIndicator))
+        UIPageControl.appearance().pageIndicatorTintColor  = UIColor(Color(.onboardColorIndicatorSet))
+       }
+        
     var body: some View {
-        VStack {
-            TabView {
-                ForEach(onboarsViews) { view in
-                    view
+        GeometryReader(content: { geometry in
+            VStack {
+                TabView(selection: $currentView,
+                        content:  {
+                    OnboardView(viewImageName: "OnboardImageOne").tag(0)
                         .onAppear(perform: {
-                            viewTitle = view.viewTitle
-                            viewText = view.viewText
+                            viewTitle = "App for the journalists"
+                            viewText = "Add and manage your arcticles"
                         })
-                    
-                }
-           }
-           .tabViewStyle(.page)
-            Spacer()
+                    OnboardView(viewImageName: "OnboardImageTwo").tag(1)
+                        .onAppear(perform: {
+                            viewTitle = "Manage your budget"
+                            viewText = "Add your statistics and earn more"
+                        })
+                    OnboardView(viewImageName: "OnboardImageThree").tag(2)
+                        .onAppear(perform: {
+                            viewTitle = "Smart notes"
+                            viewText = "Integrated notes to help you in the events"
+                        })
+                })
+                .disabled(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                .padding(.top, -geometry.safeAreaInsets.top * 2)
+                .tabViewStyle(.page)
+
+                VStack(spacing: 10, content: {
+                    Text(viewTitle)
+                            .font(.custom("", size: 28))
+                        .bold()
+                    Text(viewText)
+                })
+                VStack(content: {
+                    Button(action: {
+                        if currentView < 2 {
+                            print(currentView)
+                            currentView += 1
+                        } else {
+                            print("done")
+                        }
+                    }, label: {
+                        Text("Next")
+                            .font(.custom("", size: 17))
+                            .tint(.white)
+                            .padding(.all, 20)
+                    })
+                    .frame(maxWidth: .infinity)
+                    .background(Color(.buttonColorActive))
+                    .clipShape(.rect(cornerRadius: 20))
+                    .padding()
+                })
+            }
+        })
             
-            VStack(spacing: 10, content: {
-                Text(viewTitle)
-                        .font(.custom("", size: 28))
-                    .bold()
-                Text(viewText)
-            })
-            .padding()
-              
-        }
     }
 }
 
@@ -50,26 +80,19 @@ struct OnboardViewPresenter: View {
 struct OnboardView: View, Identifiable {
     
     var id = UUID()
-    let viewTitle: String
-    let viewText: String
     let viewImageName: String
     
     var body: some View {
-        TabView {
-            VStack {
-                GeometryReader(content: { geometry in
-                    Image(viewImageName)
-                })
-                
-
-            }
-        }
+        GeometryReader(content: { geometry in
+            Image(viewImageName)
+                .resizable()
+                .frame(width: geometry.size.width, height: 575)
+        })
+        
+        
     }
 }
 
-#Preview {
-    OnboardView(viewTitle: "App for the journalists", viewText: "Add and manage your arcticles", viewImageName: "OnboardImageOne")
-}
 
 #Preview {
     OnboardViewPresenter()
--- Sport Manager/RootView.swift
+++ Sport Manager/RootView.swift
@@ -9,23 +9,7 @@ import SwiftUI
 
 struct RootView: View {
     var body: some View {
-        VStack(content: {
-                OnboardViewPresenter()
-                .padding(.top, -110)
-
-                Button(action: {
-                    print("NEXT")
-                }, label: {
-                    Text("Next")
-                        .font(.custom("", size: 17))
-                        .tint(.white)
-                        .padding(.all, 20)
-                })
-                .frame(maxWidth: .infinity)
-                .background(Color(.blue))
-                .clipShape(.rect(cornerRadius: 20))
-                .padding()
-        })
+        OnboardViewPresenter()
     }
 }
 
