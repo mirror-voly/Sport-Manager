d964f88b21bb9dd4eb31c41df5edeb0155aadeee
nur <77068320+mirror-voly@users.noreply.github.com>
2024-07-19 11:48:53 +0400

    Initial Commit

---
+++ Sport Manager/Sport_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sport_ManagerApp.swift
+//  Sport Manager
+//
+//  Created by mix on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Sport_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
